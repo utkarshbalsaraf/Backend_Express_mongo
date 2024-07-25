e29aff2fa258c524a636c9ad4eccf7ed624932e5
utkarshbalsaraf <153362332+utkarshbalsaraf@users.noreply.github.com>
2024-07-25 22:08:45 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ import express from "express";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+const app = express();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server running at PORT: ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection fail at index", error);
+  });
 
 // const app = express();
 // (async () => {
